a9f3476505db0176a47647344c1ff4e1e8d1efd3
Mel Booth <mel.m.booth@gmail.com>
2016-10-24 21:04:58 +1300

    update server.js and install handlebars

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ app.get('/', function(req, res){
 res.send('hallelujah, it works!')
 })
 
+app.use('/public', express.static('public'))
+
 var port = Number(process.env.PORT || 3000)
 app.listen(port)
